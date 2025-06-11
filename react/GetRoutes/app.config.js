be0bf9b09a50620f33ec1384f659af813e0514cc
tak <mailtotakaf@gmail.com>
2025-06-11 21:51:16 +0900

    location登録OK

---
+++ react/GetRoutes/app.config.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'GetRoutes',
+    slug: 'getroutes',
+    version: '1.0.0',
+    sdkVersion: '50.0.0',
+    extra: {
+      POST_BUS_LOCATIONS_API_URL: process.env.POST_BUS_LOCATIONS_API_URL,
+      USER_LOCATIONS_API: process.env.USER_LOCATIONS_API,
+      // GET_ROUTES_API_URL: process.env.GET_ROUTES_API_URL,
+      COGNITO_REGION: process.env.COGNITO_REGION,
+      COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
+      COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+      DRIVER_PROF_API: process.env.DRIVER_PROF_API,
+    },
+  },
+};
