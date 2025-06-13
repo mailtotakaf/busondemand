fb3b1d5b8cd3b6d79401f16f6d1b5248f1cc05db
tak <mailtotakaf@gmail.com>
2025-06-13 19:23:46 +0900

    え

--- react/GetRoutes/app.config.js
+++ react/GetRoutes/app.config.js
@@ -7,15 +7,18 @@ export default {
     slug: 'getroutes',
     version: '1.0.0',
     sdkVersion: '53.0.0',
-    // extra: {
-    //   POST_BUS_LOCATIONS_API_URL: process.env.POST_BUS_LOCATIONS_API_URL,
-    //   USER_LOCATIONS_API: process.env.USER_LOCATIONS_API,
-    //   // GET_ROUTES_API_URL: process.env.GET_ROUTES_API_URL,
-    //   COGNITO_REGION: process.env.COGNITO_REGION,
-    //   COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
-    //   COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
-    //   DRIVER_PROF_API: process.env.DRIVER_PROF_API,
-    // },
+    extra: {
+      eas: {
+        projectId: "e8478815-902b-4fa4-8fc8-07de9f684e01"
+      }
+      // 必要なら他のextra設定もここに追加
+      // POST_BUS_LOCATIONS_API_URL: process.env.POST_BUS_LOCATIONS_API_URL,
+      // USER_LOCATIONS_API: process.env.USER_LOCATIONS_API,
+      // COGNITO_REGION: process.env.COGNITO_REGION,
+      // COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
+      // COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+      // DRIVER_PROF_API: process.env.DRIVER_PROF_API,
+    },
     plugins: [
       "expo-font",
       "expo-router",
